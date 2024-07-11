33a2ace44476b3c1ae05eb38921448584914f113
Guilherme Mormito <133173348+Mormito@users.noreply.github.com>
2024-07-11 11:03:10 -0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,12 +1,12 @@
 function somar() {
 
-    var numberOne = document.getElementById("numberOne").value;    
+    var numberOne = document.getElementById("numberOne").value;   
     var numberTwo = document.getElementById("numberTwo").value;
     
     var valor1 = parseFloat(numberOne);
     var valor2 = parseFloat(numberTwo);
     
-    var sum = valor1 + valor2;
-    
-    document.getElementById('resultado').textContent = 'Resultado: ' + sum;
+    var result = valor1 + valor2;
+
+    document.getElementById('resultado').textContent = 'Resultado: ' + result;
     }
